f86d4c836ce9d8da5540b1d1d28e8f6ba147deb7
Akashdeep <akashdeep.mandal99@gmail.com>
2022-07-09 17:12:52 +0530

    close #1; Protected Routing issue fixed

--- frontend/src/routers/ProtectedRoute.jsx
+++ frontend/src/routers/ProtectedRoute.jsx
@@ -1,15 +1,25 @@
+import { CircularProgress, Box } from "@mui/material";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 
 const ProtectedRoute = (props) => {
   const { Component } = props;
-  const { isLoggedIn } = useSelector((state) => state.user);
-  const navigate = useNavigate();
-  if (!isLoggedIn) {
-    navigate("/sign-in");
-  }
+  const { isLoading, isLoggedIn } = useSelector((state) => state.user);
+  const location = useLocation();
 
-  return <Component />;
+  const navigate = useNavigate();
+  useEffect(() => {}, [isLoading]);
+
+  return isLoggedIn ? (
+    <Component />
+  ) : isLoading === false ? (
+    <Navigate to="/unauthorized" state={{ from: location }} replace />
+  ) : (
+    <Box sx={{ display: "flex" }}>
+      <CircularProgress />
+    </Box>
+  );
 };
 
 export default ProtectedRoute;
